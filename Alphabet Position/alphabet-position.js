a8235405d058daafda6eedec7c240acd1cd27ed1
William Burdett <williamburdett@williams-air.mynet>
2022-01-15 16:34:42 +0000

    alphabet-position completed

---
+++ Alphabet Position/alphabet-position.js
@@ -0,0 +1,16 @@
+function alphabetPosition(text) {
+    var alphaArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y", "z"]
+    var inputArr = text.split("") 
+    var numArr = []  
+    for (i = 0; inputArr.length > i; i++){
+        for (j = 0; alphaArr.length > j; j++){
+            if (inputArr[i].toLowerCase() == alphaArr[j]){
+                numArr.push(j + 1)
+            } 
+        }
+    }
+    var result = numArr.join(" ")
+    return result
+  }
+
+  console.log(alphabetPosition("abcde"))
