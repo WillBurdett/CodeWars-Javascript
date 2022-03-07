570276d704fa57664e1471f07679ba177339b6d6
William Burdett <williamburdett1995@gmail.com>
2022-03-07 19:14:24 +0000

    is date today started

---
+++ IsTheDateToday/IsTheDateToday.js
@@ -0,0 +1,5 @@
+// Write a simple function that takes as a parameter a date object and 
+// returns a boolean value representing whether the date is today or not.
+function isToday(date) {
+    //Code goes here.
+  }
